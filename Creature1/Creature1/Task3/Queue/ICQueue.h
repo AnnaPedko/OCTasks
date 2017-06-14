480aad895f2aefb1697fbff1e2cd7a20d884486f
Anna P <annap@MacBook-Pro-Anna.local>
2017-06-14 18:38:26 +0300

    added queue in 6 task

---
+++ Creature1/Creature1/Task3/Queue/ICQueue.h
@@ -0,0 +1,19 @@
+//
+//  ICQueue.h
+//  Creature1
+//
+//  Created by Anna P. on 14.06.17.
+//  Copyright © 2017 Anna P. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NSObject+ICExtensions.h"
+
+@interface ICQueue : NSObject
+@property (nonatomic, retain)   NSMutableArray  *queue;
+
+- (void)enqueue:(id)object;
+- (id)dequeue;
+- (BOOL)isEmpty;
+
+@end
---
+++ Creature1/Creature1/Task3/Queue/ICQueue.m
@@ -0,0 +1,63 @@
+//
+//  ICQueue.m
+//  Creature1
+//
+//  Created by Anna P. on 14.06.17.
+//  Copyright © 2017 Anna P. All rights reserved.
+//
+
+#import "ICQueue.h"
+
+@implementation ICQueue
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (void)dealloc {
+    self.queue = nil;
+    
+    [super dealloc];
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.queue = [NSMutableArray object];
+    }
+    
+    return self;
+}
+
+#pragma mark -
+#pragma Public Methods
+
+- (void)enqueue:(id)object {
+    if (object) {
+        [self.queue addObject:object];
+     }
+}
+
+- (id)dequeue {
+    id object = [self.queue objectAtIndex:0];
+    if (object) {
+        [[object retain] autorelease];
+        [self.queue removeObjectAtIndex:0];
+    }
+    
+    return object;
+}
+
+- (BOOL)isEmpty {
+    if (![self count]) {
+        return YES;
+    } else {return NO;}
+}
+
+#pragma mark -
+#pragma Private Methods
+
+- (NSUInteger)count {
+    return self.queue.count;
+}
+
+@end
